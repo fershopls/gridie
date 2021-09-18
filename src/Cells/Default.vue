26e6f38e287ea18ff7dd76ced3c77dc8dc9f3e80
Fercho Posting <shaaf4ever@gmail.com>
2021-09-17 19:28:16 -0500

    v-bind attrs

--- src/Cells/Default.vue
+++ src/Cells/Default.vue
@@ -2,8 +2,12 @@
   <div
     v-if="context.col.html"
     v-html="value"
+    v-bind="context.col.attrs"
   ></div>
-  <div v-else>{{ value }}</div>
+  <div
+    v-else
+    v-bind="context.col.attrs"
+  >{{ value }}</div>
 </template>
 
 
