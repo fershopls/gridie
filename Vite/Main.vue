f3c9726a15bf5bb08dbf579ea3921ab1f27de960
Fercho Posting <shaaf4ever@gmail.com>
2021-09-17 19:04:09 -0500

    Vite installed

---
+++ Vite/Main.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="max-w-lg mt-12 mx-auto border border-gray-200 rounded p-6">
+    Hola
+  </div>
+</template>
+
+<script>
+export default {
+  components: {
+    //
+  },
+
+  setup() {
+    return {};
+  },
+};
+</script>
---
+++ Vite/vite.js
@@ -0,0 +1,7 @@
+import { createApp } from "vue";
+
+import App from "./Main.vue";
+
+import "./index.css";
+
+createApp(App).mount("#app");
